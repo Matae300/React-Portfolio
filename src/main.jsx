029df68099d230fe6893266984a9a73fb6edab71
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-04-16 17:16:42 -0400

    added pictures and styling to portfolio page

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
@@ -9,7 +8,6 @@ import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
 
 import './index.css'
-// Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,24 @@
+import project2 from '../assets/project2.jpeg'
+import project1 from '../assets/project1.jpeg'
+import portfolio from '../assets/HTML.png'
+
+import '../styles/Portfolio.css'
+
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
-     
+    <h1 className='portfolio'>Portfolio</h1>
+    <div className='work'>
+      <div className='work-item'>
+      <img src={project2} className="app-image" alt="Movie Mingle Project" />
+      </div>
+      <div className='work-item'>
+      <img src={project1} className="app-image" alt="Movie Mingle Project" />
+      </div>
+      <div className='work-item'>
+      <img src={portfolio} className="app-image" alt="Movie Mingle Project" />
+    </div>
+    </div>
     </div>
   );
 }
